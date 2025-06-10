f6042dfde96f35f1100a5eb41aa9ed375f98ec82
Derek Weix <47987393+dpweix@users.noreply.github.com>
2025-06-10 11:40:16 -0400

    Correct calculations to reflect percentiles of averaged L1 values by country

--- 05_tables.R
+++ 05_tables.R
@@ -6,35 +6,53 @@ library(gt)
 # Table 1 -----------------------------------------------------------------
 
 # Read in data
-df <- readRDS(here("data", "mort_temp.rds")) |> 
-  arrange(across(c(nsalid1, date))) |> 
-  # Combine Central American countries
-  mutate(country = factor(ifelse(country %in% c("PA", "GT", "SV", "CR"), "CA", country))) |> 
+df_tbl <- readRDS(here("data", "mort_temp.rds")) |> 
   # Add a copy of df with country set to "All countries" for overall calculations
   bind_rows(mutate(df, country = "All countries")) |> 
-  # Order country variable
-  mutate(country = factor(country, levels = c("All countries", "AR", "BR", "CA", "CL", "MX", "PE"))) |> 
+  # Order country variable and add full names for display in table
+  mutate(country = factor(country,
+                          levels = c("All countries", "AR", "BR", "CA", "CL", "MX", "PE"),
+                          labels = c("All countries", "Argentina", "Brazil", 
+                                     "Central America", "Chile", "Mexico", "Peru"))) |>
   arrange(country)
 
-# Number of cities, study period
-table_11 <- df |> 
+# In the following section, we create the columns of our table individually, since
+# they each require unique groupings of variables to calculate. The finished columns
+# are then combined and displayed with gt table. To make our table readable, we 
+# use the glue() function.
+
+# Columns 1-3: number of cities and study period for each country 
+table_11 <- df_tbl |> 
   summarize(n_cities = length(unique(nsalid1)), 
             study_period = glue("{year(min(date))}-{year(max(date))}"), .by = country)
 
-# City population
-table_12 <- df |> 
-  summarize(pop = first(pop), .by = c(country, nsalid1, sex, age)) |> 
-  summarize(city_pop = sum(pop)/1000, .by = c(country, nsalid1)) |> 
+# Column 4: city population
+table_12 <- df_tbl |> 
+  # Our population variable changes each year, so we add a year variable
+  mutate(year = year(date)) |> 
+  # We extract the first observation for each country:L1:year:sex:age grouping.
+  # This will allow us to sum up all of our city population observations without
+  # adding repeated measurements.
+  summarize(pop = first(pop), .by = c(country, nsalid1, year, sex, age)) |> 
+  # We sum to get the total city population (in thousands) for each L1 and year
+  summarize(city_pop = sum(pop)/1000, .by = c(country, nsalid1, year)) |> 
+  # Because the population changes over the years, we take the average across all
+  # years for each L1.
+  summarize(city_pop = mean(city_pop), .by = c(country, nsalid1)) |> 
+  # Now we extract the median, 10th and 90th percentile city average populations
+  # by country
   summarize(city_pop = glue("{med} ({lower}, {upper})",
                             med   = round(median(city_pop), 0),
                             lower = round(quantile(city_pop, .1), 0),
                             upper = round(quantile(city_pop, .9), 0)), .by = country)
 
 # Percentage aged >= 65
-table_13 <- df |>
-  summarize(pop = first(pop), .by = c(country, nsalid1, sex, age)) |> 
+table_13 <- df_tbl |>
+  mutate(year = year(date)) |> 
+  summarize(pop = first(pop), .by = c(country, nsalid1, year, sex, age)) |> 
   mutate(age = fct_collapse(age, pop_over_65 = "65+", other_level = "pop_under_65")) |> 
-  summarize(total_pop = sum(pop), .by = c(country, nsalid1, age)) |> 
+  summarize(total_pop_yearly = sum(pop), .by = c(country, nsalid1, year, age)) |> 
+  summarize(total_pop = mean(total_pop_yearly), .by = c(country, nsalid1, age)) |> 
   pivot_wider(names_from = age, values_from = total_pop) |> 
   mutate(pct_over_65 = 100*pop_over_65/(pop_under_65 + pop_over_65)) |> 
   summarize(pct_over_65 = glue("{med} ({lower}, {upper})",
@@ -43,23 +61,25 @@ table_13 <- df |>
                                upper = round(quantile(pct_over_65, .9), 1)), .by = country)
 
 # Annual deaths
-table_14 <- df |> 
+table_14 <- df_tbl |> 
   mutate(year = year(date)) |> 
   summarize(annual_deaths = sum(deaths), .by = c(country, nsalid1, year)) |> 
+  summarize(annual_deaths = mean(annual_deaths), .by = c(country, nsalid1)) |> 
   summarize(annual_deaths = glue("{med} ({lower}, {upper})",
                                  med   = round(median(annual_deaths), 0),
                                  lower = round(quantile(annual_deaths, .1), 0),
                                  upper = round(quantile(annual_deaths, .9), 0)), .by = country)
 
 # Mean temperature
-table_15 <- df |> 
+table_15 <- df_tbl |> 
   summarize(tmean = first(tmean), .by = c(country, nsalid1, date)) |> 
+  summarize(tmean = mean(tmean), .by = c(country, nsalid1)) |> 
   summarize(tmean = glue("{med} ({lower}, {upper})",
                          med   = round(median(tmean), 1),
                          lower = round(quantile(tmean, .1), 1),
                          upper = round(quantile(tmean, .9), 1)), .by = country)
 
-# Combine all table
+# Combine all tables
 list_table_1 <- list(table_11, table_12, table_13, table_14, table_15)
 
 list_table_1 |> 
@@ -81,9 +101,10 @@ list_table_1 |>
 
 # Table 2 -----------------------------------------------------------------
 
-
+# Load EDFs
 df_EDF <- readRDS(here("data", "EDFs.rds"))
 
+# Create table 2
 df_EDF |> 
   summarize(across(contains("range"),
                    \(x) {
